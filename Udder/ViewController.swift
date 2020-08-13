8b5c81ff6f1cd045e7b6862758a44b420b197a28
Ameya Bhagat <ameya.bhagat@outlook.com>
2020-08-13 17:47:42 +0530

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Ameya Bhagat on 13/08/20.
+//  Copyright © 2020 Ameya Bhagat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
